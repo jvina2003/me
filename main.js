2dd5d88154d7e17d402e4c73854ae9f02f1a6968
jvina2003 <jvina2003@outlook.com>
2022-05-04 12:35:14 -0500

    cooolors!!

--- main.js
+++ main.js
@@ -1,7 +1,35 @@
+function bodyPaint() {
+	window.addEventListener("scroll", function () {
+		const scrollTop =
+			window.pageYOffset ||
+			document.documentElement.scrollTop ||
+			document.body.scrollTop ||
+			0;
 
-const body = document.querySelector('body');
-const toggle = document.getElementById('toggle');
-toggle.onclick = function(){ 
-    toggle.classList.toggle('active');
-    body.classList.toggle('active');
+		const triggers = document.querySelectorAll("[data-color]");
+
+		const body = document.body;
+
+		// Change 33% earlier than scroll position so colour is there when you arrive.
+		const scroll = scrollTop + window.innerHeight / 3;
+
+		for (var i = 0; i < triggers.length; i++) {
+			const trigger = triggers[i];
+			const triggerRect = trigger.getBoundingClientRect();
+			const triggerRectTop = triggerRect.top + scrollTop;
+			const triggerHeight = trigger.offsetHeight;
+			const triggerColor = trigger.getAttribute("data-color");
+
+			// if position is within range of this panel.
+			// So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
+			// Remember we set the scroll to 33% earlier in scroll var.
+			if (triggerRectTop <= scroll && triggerRectTop + triggerHeight > scroll) {
+				// Set attribute of currently active div
+				body.setAttribute("data-paint", triggerColor);
+				console.log(body.dataset.paint);
+			}
+		}
+	});
 }
+
+bodyPaint();
